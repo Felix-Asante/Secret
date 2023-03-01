f78dbeaae28f22d298fe7c9683b0ce0c3e59a128
Felix-Asante <quamzgraphix@gmail.com>
2023-03-01 21:56:45 +0100

    account card modified

--- src/components/ui/cards/AccountCard.tsx
+++ src/components/ui/cards/AccountCard.tsx
@@ -4,12 +4,12 @@ import {Image} from 'react-native';
 
 export default function AccountCard() {
   return (
-    <View className="bg-light p-3 rounded-md  flex-row items-center gap-x-3 mb-3">
+    <View className="bg-light py-3 px-1 rounded-md  flex-row items-center gap-x-3 mb-3">
       <Image
         source={require('../../../../assets/artist.png')}
         className="w-10 h-10 rounded-full"
       />
-      <View className="w-[88%]">
+      <View className="w-[78%]">
         <View className="flex-row w-full justify-between">
           <Text className="font-semibold">Account Name</Text>
           <View className="bg-gray100 rounded-full py-1 px-3 ">
